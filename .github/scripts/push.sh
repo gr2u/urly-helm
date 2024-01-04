8a24e7e0af7261f1fabb66bc0f209cad3627a158
gr2u <gluchio@gmail.com>
2024-01-04 18:13:18 +0200

    docs: docs

--- .github/scripts/push.sh
+++ .github/scripts/push.sh
@@ -2,5 +2,5 @@
 echo "::group::Pushing helm package to registry"
 export HELM_REGISTRY=$(node -p "require('./package.json').helm.registry");
 export HELM_EXPERIMENTAL_OCI=1
-helm push ./* oci://"${HELM_REGISTRY}"
+helm push ./urly* oci://"${HELM_REGISTRY}"
 echo "::endgroup::"
